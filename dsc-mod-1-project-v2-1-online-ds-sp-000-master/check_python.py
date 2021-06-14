ef9049bbe2d3e06f68b3f3d9188663fe05e9bb47
Claire Sarraille <clairesarraille@outlook.com>
2021-06-14 01:44:04 -0700

    All visualizations are complete except production companies

--- dsc-mod-1-project-v2-1-online-ds-sp-000-master/check_python.py
+++ dsc-mod-1-project-v2-1-online-ds-sp-000-master/check_python.py
@@ -1,3 +1,9 @@
-df_q3['revenue_millions'] = '$' + (df_q3['revenue'].astype(round(float)/1000000, 2).astype(str) + 'MM'
-
-df['($) millions'] = '$' + (df['Amount'].astype(round(float,2))/1000000).astype(str) + 'MM'
+def boolean_df(item_lists, unique_items):
+    # Instantiate Boolean Dictionary:
+    bool_dict = {}
+    # Loop through all the tags
+    for i, item in enumerate(unique_items):
+        # Apply boolean mask that returns a True-False list of whether a tag is in a taglist
+        bool_dict[item] = item_lists.apply(lambda x: item in x)
+    # Return the results as a dataframe
+    return pd.DataFrame(bool_dict)
