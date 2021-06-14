bc0a7951749200a7fc5dee2ded507ff4278ea1ce
Claire Sarraille <clairesarraille@outlook.com>
2021-06-14 02:53:35 -0700

    removed extraneous files and completed all visualizations

--- dsc-mod-1-project-v2-1-online-ds-sp-000-master/check_python.py
+++
@@ -1,9 +0,0 @@
-def boolean_df(item_lists, unique_items):
-    # Instantiate Boolean Dictionary:
-    bool_dict = {}
-    # Loop through all the tags
-    for i, item in enumerate(unique_items):
-        # Apply boolean mask that returns a True-False list of whether a tag is in a taglist
-        bool_dict[item] = item_lists.apply(lambda x: item in x)
-    # Return the results as a dataframe
-    return pd.DataFrame(bool_dict)
--- the_numbers_data/read_html_example.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_html(
-    'http://www.basketball-reference.com/boxscores/200112100LAC.html')[0]  # or [1], [2]
-print(df)
